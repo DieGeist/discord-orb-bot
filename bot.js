e37d7c04ce3ca2f14b53c3e68a7d490f00792ef5
Ross Francis <rossfrancis@comcast.net>
2025-09-05 19:42:10 -0400

    Simplify token usage back to DISCORD_TOKEN

--- bot.js
+++ bot.js
@@ -59,15 +59,5 @@ client.on('messageCreate', (message) => {
     }
 });
 
-// Debug logging
-console.log('Checking for bot token...');
-console.log('DISCORD_TOKEN exists:', !!process.env.DISCORD_TOKEN);
-console.log('BOT_TOKEN exists:', !!process.env.BOT_TOKEN);
-
 // Login to Discord with your bot's token from environment variable
-const token = process.env.BOT_TOKEN || process.env.DISCORD_TOKEN;
-if (!token) {
-    console.error('No bot token found! Please check your environment variables.');
-    process.exit(1);
-}
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
